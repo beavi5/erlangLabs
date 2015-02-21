352d47e2ff77bde1d79b4717b94777638e4ddd9c
beavi5 <beavi5@list.ru>
2015-02-21 21:56:50 +0500

    +summa and price

--- matt.erl
+++ matt.erl
@@ -1,6 +1,6 @@
 
 -module(matt).
--export([fac/1,fib/1,per/1,area/1,memberof/2]).
+-export([fac/1,fib/1,per/1,area/1,memberof/2, price/1,summa/1]).
 fac(0)->1;
 fac(N)->N*fac(N-1).
 fib(0)->0;
@@ -30,10 +30,11 @@ area(Fig)-> case  Fig of
 	{circle, A}  -> 3.14*A*A
 end.
 
-memberof(O,[])->false;
+
+
+memberof(O,[])->fals;
 
 memberof(O,[H|T]) -> 
-
 
 case H of
 	[] -> false;
@@ -43,4 +44,21 @@ case H of
 		[]-> false;
 		_->memberof(O,T)
 		end
+end.
+
+
+
+price('pivo') ->50;
+price('riba') ->15;
+price('myaso') ->100;
+price('hleb') ->20.
+
+summa([])->0;
+
+summa([{X,Y}|T]) -> 
+
+case [{X,Y}] of
+	[] -> 0;
+	_-> price(X)*Y+ summa(T)
+		
 end.
